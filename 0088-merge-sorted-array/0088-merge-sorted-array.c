29597c810a7f1c0e9d02617d2bb39d473784ba7b
Aziz Ben Lamine <85827109+Aziz-BenLamine@users.noreply.github.com>
2023-10-17 16:24:17 +0100

    Time: 5 ms (50.69%), Space: 6.8 MB (18.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -1,26 +1,11 @@
-void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
-    int i = m-1; //Last element index in nums1
-    int j = n-1; //Last element index in nums2
-    int k = m+n -1;//Last element in merged array
-
-    while(i >= 0 && j >= 0){
-        if(nums1[i]>nums2[j]){
-            nums1[k] = nums1[i];
-            i--;
-        }
-        else{
-            nums1[k] = nums2[j];
-            j--;
-        }
-        k--;
-    }
-
-    while(j >= 0){
-        nums1[k]= nums2[j];
-        j--;
-        k--;
-    }
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
 }
 
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
 
-
+    for(int i = m; i < m+n; i++){
+        nums1[i] = nums2[i-m];
+    }
+    qsort(nums1, m+n, sizeof(int), compare);
+}
