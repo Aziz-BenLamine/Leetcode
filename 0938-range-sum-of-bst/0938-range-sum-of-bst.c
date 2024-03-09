e3f7e3d8e8a39645de8ca8955305d14331faeaf2
Aziz Ben Lamine <85827109+Aziz-BenLamine@users.noreply.github.com>
2024-03-09 09:57:03 +0100

    Time: 71 ms (31.91%), Space: 42.6 MB (74.31%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.c
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.c
@@ -7,18 +7,8 @@
  * };
  */
 int rangeSumBST(struct TreeNode* root, int low, int high) {
-    if(root == NULL){
-        return 0;
-    }
-    struct TreeNode *node = root;
-    if(node->val >= low && node->val <= high){
-        return node->val + rangeSumBST(root->right, low, high) + rangeSumBST(root->left, low, high);
-    }
-    if(root->val < low){
-        return rangeSumBST(root->right,low,high);
-    }
-    if(root->val > high){
-        return rangeSumBST(root->left,low,high);
-    }
-    return 0;
+    if (root == NULL) return 0;
+    if(root->val > high) return rangeSumBST(root->left , low, high);
+    if(root->val < low) return rangeSumBST(root->right, low, high);
+    return root->val + rangeSumBST(root->left , low, high) + rangeSumBST(root->right, low, high);
 }
